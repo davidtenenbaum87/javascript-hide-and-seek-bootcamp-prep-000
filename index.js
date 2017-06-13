ad5dbdf35d0259e217f4dda665869d4fa8fc574a
David Tenenbaum <david.tenenbaum87@gmail.com>
2017-06-13 21:06:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + parseInt(n);
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
